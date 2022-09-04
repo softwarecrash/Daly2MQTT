4f6eae39f73cc02adb88562a1c9476553dd5a2eb
softwarecrash <lacrimosa2k2@gmail.com>
2022-09-04 09:02:31 +0200

    version bump

--- src/webpages/htmlCase.h
+++ src/webpages/htmlCase.h
@@ -25,7 +25,7 @@ const char HTML_HEAD[] PROGMEM = R"rawliteral(
 )rawliteral";
 
 const char HTML_FOOT[] PROGMEM = R"rawliteral(
-        <figure class="text-center">DALY BMS to MQTT V0.3.7 By <a href="https://github.com/softwarecrash/"
+        <figure class="text-center">DALY BMS to MQTT V0.3.8 By <a href="https://github.com/softwarecrash/"
                 target="_blank">Softwarecrash</a></figure>
     </div>
 </body>
