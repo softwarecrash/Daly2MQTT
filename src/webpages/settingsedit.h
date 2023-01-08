08ab03506798b1ff063a0900e833b252ff3af5f9
Daniel <dloos.westheim@gmail.com>
2023-01-08 16:39:52 +0100

    NEW: settingsedit.h: added settings for BMS Wakeup and Relais Output NEW: settings.h: added settings for BMS Wakeup and Relais Output NEW: main.ino: changed settings handling to handle the new values FIX: main.ino: workaround for soc and mos values resetting to mqtt values FIX: settings.h: fixed boolean handling in sanitycheck()

--- src/webpages/settingsedit.h
+++ src/webpages/settingsedit.h
@@ -52,6 +52,71 @@ const char HTML_SETTINGS_EDIT[] PROGMEM = R"rawliteral(
                 role="switch" id="mqttjson" name="post_mqttjson" value="true" />
         </div>
     </div>
+    <!-- ADDED BY derLoosi START -->
+    <div class="input-group mb-2">
+        <span class="input-group-text w-100"><b>BMS-Wakeup Settings</b></span>
+    </div>
+	
+	<div class="input-group mb-2">
+        <span class="input-group-text w-50" id="wakeupenabledesc">Enable BMS Wakeup GPIO13 (D7)</span>
+		<div class="form-switch form-control mqtt-settings-switch" style="width:50%%; text-align: center;">
+            <input type="checkbox" class="form-check-input form control" aria-describedby="wakeupenabledesc"
+                role="switch" id="wakeupenable" name="post_wakeupenable" value="true" />
+        </div>
+    </div>
+
+    <div class="input-group mb-2">
+        <span class="input-group-text w-50" id="wakeupinvertdesc">Invert GPIO Output</span>
+		<div class="form-switch form-control mqtt-settings-switch" style="width:50%%; text-align: center;">
+            <input type="checkbox" class="form-check-input form control" aria-describedby="wakeupinvertdesc"
+                role="switch" id="wakeupinvert" name="post_wakeupinvert" value="true" />
+        </div>
+    </div>
+
+	<div class="input-group mb-2">
+        <span class="input-group-text w-100"><b>Output Settings</b></span>
+    </div>
+	
+	<div class="input-group mb-2">
+        <span class="input-group-text w-50" id="relaisenabledesc">Enable Output on GPIO15 (D8)</span>
+		<div class="form-switch form-control mqtt-settings-switch" style="width:50%%; text-align: center;">
+            <input type="checkbox" class="form-check-input form control" aria-describedby="relaisenabledesc"
+                role="switch" id="relaisenable" name="post_relaisenable" value="true" />
+        </div>
+    </div>
+	
+	<div class="input-group mb-2">
+        <span class="input-group-text w-50" id="relaisinvertdesc">Invert GPIO Output</span>
+		<div class="form-switch form-control mqtt-settings-switch" style="width:50%%; text-align: center;">
+            <input type="checkbox" class="form-check-input form control" aria-describedby="relaisinvertdesc"
+                role="switch" id="relaisinvert" name="post_relaisinvert" value="true" />
+        </div>
+    </div>
+	
+	<div class="input-group mb-2">
+        <span class="input-group-text w-50" id="relaisfunctiondesc">Function</span>
+        <select class="form-select" aria-describedby="relaisfunctiondesc" id="relaisfunction" name="post_relaisfunction">
+             <option value="0">Lowest Cell Voltage</option>
+			 <option value="1">Highest Cell Voltage</option>
+			 <option value="2">Pack Cell Voltage</option>
+			 <option value="3">Temperature</option>
+		</select>
+    </div>
+	
+	<div class="input-group mb-2">
+        <span class="input-group-text w-50" id="relaiscomparsiondesc">Comparsion</span>
+        <select class="form-select" aria-describedby="relaiscomparsiondesc" id="relaiscomparsion" name="post_relaiscomparsion">
+             <option value="0">Higher or equal than</option>
+			 <option value="1">Lower or equal than</option>
+		</select>
+    </div>
+	
+	<div class="input-group mb-2">
+        <span class="input-group-text w-50" id="relaissetvaluedesc">Value</span>
+        <input type="number" step="0.01" class="form-control" aria-describedby="relaissetvaluedesc" id="relaissetvalue" maxlength="4"
+            name="post_relaissetvalue" value="">
+    </div>
+	<!-- ADDED BY derLoosi END -->
 
     <div class="d-grid gap-2">
         <input class="btn btn-primary" type="submit" value="Save settings">
@@ -75,6 +140,13 @@ const char HTML_SETTINGS_EDIT[] PROGMEM = R"rawliteral(
                 document.getElementById("mqttpassword").value = data.mqtt_password;
                 document.getElementById("mqttrefresh").value = data.mqtt_refresh;
                 document.getElementById("mqttjson").checked = data.mqtt_json;
+                document.getElementById("wakeupenable").checked = data.wakeup_enable;
+                document.getElementById("wakeupinvert").checked = data.wakeup_invert;
+                document.getElementById("relaisenable").checked = data.relais_enable;
+                document.getElementById("relaisinvert").checked = data.relais_invert;
+                $("#relaisfunction").val(data.relais_function);
+                $("#relaiscomparsion").val(data.relais_comparsion);
+                document.getElementById("relaissetvalue").value = data.relais_setvalue;
             }
         });
     });
