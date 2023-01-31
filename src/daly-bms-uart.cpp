86640e14fcc45fa2aebb7041de1ff5ca528572b0
Softwarecrash <lacrimosa2k2@gmail.com>
2023-01-31 19:11:19 +0100

    changes

--- src/daly-bms-uart.cpp
+++ src/daly-bms-uart.cpp
@@ -338,7 +338,7 @@ bool Daly_BMS_UART::getCellTemperature() // 0x96
         for (size_t i = 0; i < 7; i++)
         {
             DEBUG_PRINT("<DALY-BMS DEBUG> Frame No.: " + (String)this->my_rxBuffer[4]);
-            DEBUG_PRINTLN(" Sensor No: " + (String)(sensorNo + 1) + ". " + String(this->my_rxBuffer[5 + i] - 40) + "°C");
+            DEBUG_PRINTLN(" Sensor No: " + (String)(sensorNo + 1) + ". " + String(this->my_rxBuffer[5 + i] - 40) + char(176)+"C");
             get.cellTemperature[sensorNo] = (this->my_rxBuffer[5 + i] - 40);
             sensorNo++;
             if (sensorNo >= get.numOfTempSensors)
