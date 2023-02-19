e1af086336aba7d71cb9121724b4a13e4a5fe299
ManuA <webmaster@all-solutions.de>
2023-02-19 19:31:27 +0100

    cleanup html & js

--- src/webpages/main.h
+++ src/webpages/main.h
@@ -97,7 +97,17 @@ const char HTML_MAIN[] PROGMEM = R"rawliteral(
     </div>
 </div>
 
-%MAIN_RELAIS%
+<div class="row gx-0 mb-2" id="esp01" style="display: %ESP01%;">
+    <div class="row gx-0 mb-2">
+        <div class="col">
+            <div class="bg-light">Relais Output: </div>
+        </div>
+        <div class="col">
+            <div class="bg-light form-check form-switch"><input class="form-check-input" type="checkbox" role="switch"
+                    id="relaisOutputActive" disabled></div>
+        </div>
+    </div>
+</div>
 
 <div class="d-grid gap-2">
     <a class="btn btn-primary btn-block" href="/settings" role="button">Settings</a>
@@ -159,7 +169,6 @@ const char HTML_MAIN[] PROGMEM = R"rawliteral(
         }else{
             document.getElementById("vcc_alert").style.display = 'none';
         }
-
     }
 
     function onLoad(event) {
