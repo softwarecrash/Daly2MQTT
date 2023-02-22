9acf74c170bb55522ba993bb5a05da936c42553f
softwarecrash <lacrimosa2k2@gmail.com>
2023-02-22 11:45:36 +0100

    something

--- src/status-LED.h
+++ src/status-LED.h
@@ -8,4 +8,25 @@
 
 void notificationLED(){
 
+
+
+  unsigned long currentMillis = millis();
+
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+
+    // if the LED is off turn it on and vice-versa:
+    if (ledState == LOW) {
+      ledState = HIGH;
+    } else {
+      ledState = LOW;
+    }
+
+    // set the LED with the ledState of the variable:
+    digitalWrite(ledPin, ledState);
+  }
+
+
+
 }
