77d5d1a01896cc823f7dae82101d19e6c34f8a1d
softwarecrash <lacrimosa2k2@gmail.com>
2023-02-23 14:54:54 +0100

    something add

--- src/status-LED.h
+++ src/status-LED.h
@@ -1,9 +1,5 @@
 /***
- 1 mal blinken status, alles ok
- 2 mal blinken bms fehler
- 3 mal blinken mqtt fehler
- 4 mal blinken wlan fehler
-
+https://wokwi.com/projects/357439918883980289
 */
 
 
@@ -13,30 +9,7 @@ void notificationLED(){
 
 if(bms.get.chargeDischargeStatus == "offline")
 {
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
+
 }
-/*
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
-  }
-*/
-
  digitalWrite(LED_PIN, ledState);
 }
