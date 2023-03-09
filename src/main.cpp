eb0c9dc027ac4a0157de0e6fe24bfd5d3c0d3093
Softwarecrash <lacrimosa2k2@gmail.com>
2023-03-09 17:16:32 +0100

    something

--- src/main.cpp
+++ src/main.cpp
@@ -755,16 +755,15 @@ bool sendtoMQTT()
 
     for (size_t i = 0; i < bms.get.numberOfCells; i++)
     {
-     // mqttclient.publish(topicBuilder(buff,(const char *)("Pack_Cells_Voltage/Cell_" + i + 1)), (const char *)dtostrf(bms.get.cellVmV[i] / 1000, 5, 3, msgBuffer));
-    //  mqttclient.publish(topicBuilder(buff,(const char *)("Pack_Cells_Balance/Cell_" + i + 1)), (const char *)bms.get.cellBalanceState[i] ? "true" : "false");
-      //mqttclient.loop();
-    }
-
-    for (unsigned int i = 0; i < 10; i++)
-    {
       mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Voltage/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)dtostrf(bms.get.cellVmV[i] / 1000, 5, 3, msgBuffer));
       mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Balance/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)bms.get.cellBalanceState[i] ? "true" : "false");
     }
+
+  //  for (unsigned int i = 0; i < 10; i++)
+  //  {
+  //    mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Voltage/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)dtostrf(bms.get.cellVmV[i] / 1000, 5, 3, msgBuffer));
+  //    mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Balance/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)bms.get.cellBalanceState[i] ? "true" : "false");
+   // }
 
 
     for (size_t i = 0; i < bms.get.numOfTempSensors; i++)
