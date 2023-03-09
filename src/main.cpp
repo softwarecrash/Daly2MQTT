a32d24dcaa6cc02a13f12c68d59842b20d13d8bb
Softwarecrash <lacrimosa2k2@gmail.com>
2023-03-09 17:26:25 +0100

    something else...

--- src/main.cpp
+++ src/main.cpp
@@ -746,29 +746,17 @@ bool sendtoMQTT()
     mqttclient.publish(topicBuilder(buff,"Pack_DischargeFET"), (const char *)bms.get.disChargeFetState ? "true" : "false");
     mqttclient.publish(topicBuilder(buff,"Pack_Status"), (const char *)bms.get.chargeDischargeStatus);
     mqttclient.publish(topicBuilder(buff,"Pack_Cells"), (const char *)itoa(bms.get.numberOfCells, msgBuffer, 10));
-
     mqttclient.publish(topicBuilder(buff,"Pack_Heartbeat"), itoa(bms.get.bmsHeartBeat, msgBuffer, 10));
-
-
     mqttclient.publish(topicBuilder(buff,"Pack_Balance_Active"), (const char *)bms.get.cellBalanceActive ? "true" : "false");
-
 
     for (size_t i = 0; i < bms.get.numberOfCells; i++)
     {
-      mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Voltage/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)dtostrf(bms.get.cellVmV[i] / 1000, 5, 3, msgBuffer));
-      mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Balance/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)bms.get.cellBalanceState[i] ? "true" : "false");
+      mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Voltage/Cell_"),itoa((i + 1), msgBuffer, 10)), (const char *)dtostrf(bms.get.cellVmV[i] / 1000, 5, 3, msgBuffer));
+      mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Balance/Cell_"),itoa((i + 1), msgBuffer, 10)), (const char *)bms.get.cellBalanceState[i] ? "true" : "false");
     }
-
-  //  for (unsigned int i = 0; i < 10; i++)
-  //  {
-  //    mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Voltage/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)dtostrf(bms.get.cellVmV[i] / 1000, 5, 3, msgBuffer));
-  //    mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cells_Balance/Cell1_"),itoa((i + 1), msgBuffer, 10)), (const char *)bms.get.cellBalanceState[i] ? "true" : "false");
-   // }
-
-
     for (size_t i = 0; i < bms.get.numOfTempSensors; i++)
     {
-      mqttclient.publish(topicBuilder(buff,"Pack_Cell_Temperature_" + char(i + 1)), (const char *)itoa(bms.get.cellTemperature[i], msgBuffer, 10));
+      mqttclient.publish(strcat(topicBuilder(buff,"Pack_Cell_Temperature_"),itoa((i + 1), msgBuffer, 10)), (const char *)itoa(bms.get.cellTemperature[i], msgBuffer, 10));
     }
   }
   else
