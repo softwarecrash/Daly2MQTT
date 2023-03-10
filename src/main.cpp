b982dd22e2139f07ad8f17195601eb77ae22b1b5
ManuA <webmaster@all-solutions.de>
2023-03-10 17:38:48 +0100

    added some more esp-infos in debug mode and rssi level

--- src/main.cpp
+++ src/main.cpp
@@ -663,16 +663,23 @@ void prozessUartData()
 void getJsonDevice()
 {
   deviceJson[F("ESP_VCC")] = ESP.getVcc() / 1000.0;
+  deviceJson[F("Wifi_RSSI")] = WiFi.RSSI();
   deviceJson[F("Relais_Active")] = relaisComparsionResult ? true : false;
   deviceJson[F("Relais_Manual")] = _settings.data.relaisEnable && _settings.data.relaisFunction == 4 ? true : false;
 #ifdef DALY_BMS_DEBUG
+//  deviceJson[F("Wifi_RSSI")] = WiFi.RSSI();
+  deviceJson[F("CPU_Frequency")] = ESP.getCpuFreqMHz();
+  deviceJson[F("Real_Flash_Size")] = ESP.getFlashChipRealSize();
+  deviceJson[F("Flash_Size")] = ESP.getFlashChipSize();
+  deviceJson[F("Sketch_Size")] = ESP.getSketchSize();
+  deviceJson[F("Free_Sketch_Space")] = ESP.getFreeSketchSpace();
   deviceJson[F("Free_Heap")] = ESP.getFreeHeap();
+  deviceJson[F("HEAP_Fragmentation")] = ESP.getHeapFragmentation();
+  deviceJson[F("Free_BlockSize")] = ESP.getMaxFreeBlockSize();
   deviceJson[F("json_memory_usage")] = bmsJson.memoryUsage();
   deviceJson[F("json_capacity")] = bmsJson.capacity();
   deviceJson[F("runtime")] = millis() / 1000;
   deviceJson[F("ws_clients")] = ws.count();
-  deviceJson[F("HEAP_Fragmentation")] = ESP.getHeapFragmentation();
-  deviceJson[F("free_blocksize")] = ESP.getMaxFreeBlockSize();
 #endif
 }
 
