97f2612b6fbf18edbc5e2079cff79eb02beea8cb
softwarecrash <lacrimosa2k2@gmail.com>
2023-07-07 19:56:41 +0200

    update reboot page

--- src/webpages/reboot.h
+++ src/webpages/reboot.h
@@ -3,16 +3,29 @@ DALY2MQTT Project
 https://github.com/softwarecrash/DALY2MQTT
 */
 const char HTML_REBOOT[] PROGMEM = R"rawliteral(
-    %HEAD_TEMPLATE%
-<figure class="text-center"><h1>Rebooting ...</h1></figure>
+%HEAD_TEMPLATE%
+<figure class="text-center"><h1>Rebooting </h1><h2 id="wait">.</h2></figure>
 <div class="d-grid gap-2">
 <a class="btn btn-primary" href="/" role="button">Main</a>
 </div>
 
 <script>
 $(document).ready(function () {
-setTimeout(startCheck, 2000);
+    window.dotsGoingUp = true;
+    var dots = window.setInterval( function() {
+        var wait = document.getElementById("wait");
+        if ( window.dotsGoingUp ) 
+            wait.innerHTML += ".";
+        else {
+            wait.innerHTML = wait.innerHTML.substring(1, wait.innerHTML.length);
+            if ( wait.innerHTML === ".")
+                window.dotsGoingUp = true;
+        }
+        if ( wait.innerHTML.length > 9 )
+            window.dotsGoingUp = false;
+        }, 100);
 
+setTimeout(startCheck, 2000);
 
 function startCheck(){
   setInterval(serverReachable, 1000);
