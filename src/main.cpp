c2d3a65267585bb87ecaabdc5a252eae6b78dacb
softwarecrash <lacrimosa2k2@gmail.com>
2023-08-05 14:14:07 +0200

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -16,11 +16,6 @@ https://github.com/softwarecrash/DALY2MQTT
 #include <Updater.h> //new
 #include "Settings.h"
 
-//#include "webpages/htmlCase.h"      // The HTML Konstructor
-//#include "webpages/main.h"          // landing page with menu
-//#include "webpages/settings.h"      // settings page
-//#include "webpages/settingsedit.h"  // mqtt settings page
-//#include "webpages/reboot.h"        // Reboot Page
 #include "html.h"
 #include "htmlProzessor.h" // The html Prozessor
 
