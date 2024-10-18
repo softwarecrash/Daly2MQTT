fb7dbb9f913c4aa8849aead1e03e4a33a41a576f
ManuA <17761850+all-solutions@users.noreply.github.com>
2024-10-18 12:18:22 +0200

    Reworked variables used
    Rework of the variables used to better identify the versions

--- tools/pre_compile.py
+++ tools/pre_compile.py
@@ -2,7 +2,7 @@ Import("env")
 
 env.Append(CPPDEFINES=[
     ("SWVERSION", env.StringifyMacro(env.GetProjectOption("custom_prog_version"))),
-    ("HWBOARD", env.StringifyMacro(env["BOARD"])),
+    ("HWBOARD", env.StringifyMacro(env["PIOENV"])),
 ])
 
-env.Replace(PROGNAME="Daly2MQTT_%s_%s" % (str(env["BOARD"]), env.GetProjectOption("custom_prog_version")))
+env.Replace(PROGNAME="Daly2MQTT_%s_%s" % (str(env["PIOENV"]), env.GetProjectOption("custom_prog_version")))
